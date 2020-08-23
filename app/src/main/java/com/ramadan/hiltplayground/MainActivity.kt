fa73a04d97547f8383477c22089d7530fa882aef
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2020-08-24 00:13:17 +0200

    add space

--- app/src/main/java/com/ramadan/hiltplayground/MainActivity.kt
+++ app/src/main/java/com/ramadan/hiltplayground/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         aClass.doSomething()
         aClass.doAnotherThing()
     }
